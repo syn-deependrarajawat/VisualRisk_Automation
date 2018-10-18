f09f3eea71c3338f798fcf270f7764f6b0973d1a
syn-deependrarajawat <43608922+syn-deependrarajawat@users.noreply.github.com>
2018-10-18 15:26:07 +0530

    Newly created object repository

---
+++ VisualRisk_Automation/src/test/java/objectRepository/DashBoard_OR.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package objectRepository;
+
+import org.openqa.selenium.By;
+
+/**
+ * @author Deependra.Rajawat
+ *
+ */
+public class DashBoard_OR {
+
+	public static final By bySubmissionGridRows = By.xpath("//*[contains(text(),'Submissions')]/../following-sibling::div//table/tbody/tr[]//a");
+}
