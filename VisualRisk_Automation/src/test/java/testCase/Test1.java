caa2c55fc0b85dde2b3b22bb16e9a59f5643a2b7
syn-deependrarajawat <43608922+syn-deependrarajawat@users.noreply.github.com>
2018-10-29 20:31:36 +0530

    Added a new test case

---
+++ VisualRisk_Automation/src/test/java/testCase/Test1.java
@@ -0,0 +1,42 @@
+/**
+ * @TCName:- EndToEndValidation_ExistingForm_Test
+ * @Description:- This test case verify the E2E flow for an existing form
+ * @author Deependra.Rajawat
+ * @CreationDate:	01-Oct-2018
+ */
+
+package testCase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import library.BaseTest;
+import library.CustomListener;
+
+@Listeners(CustomListener.class)
+public class Test1 extends BaseTest {
+
+	static WebDriver driver;
+	static WebElement element;
+
+	@Test
+	public static void endToEndValidation_ExistingForm_Test() throws Exception {
+
+		commonLib.waitUntilUnmask(120);
+		commonLib.clickOnSubmissionsNoLink(2);
+		commonLib.waitUntilUnmask(120);
+//		commonLib.clickOnSaveButton();
+		commonLib.waitUntilUnmask(120);
+		commonLib.waitUntilWidgetUnmask(3);
+		commonLib.clickOnTopMenu("Insights");
+		commonLib.getNoOfWidgetsOnThePage( "Insights");
+		commonLib.clickOnTopMenu("Location");
+		commonLib.getNoOfWidgetsOnThePage("Location");
+		commonLib.clickOnTopMenu("Insights");
+		commonLib.clickOnWidget("BAREBURGER.COM");
+		commonLib.closeWidget();
+		commonLib.logOut();
+	}
+}
